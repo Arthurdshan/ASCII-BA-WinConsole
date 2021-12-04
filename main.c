80dce2104fdc4aadcc334589e40669e5e3c9b1cf
Arthur <arthurhan269@gmail.com>
2021-12-04 02:42:11 -0300

    added time count, frames count and title

--- main.c
+++ main.c
@@ -10,6 +10,28 @@
 #define PROJECT_NAME "C Project - [Touhou] Bad Apple!! [ASCII ART]"
 #define rcrs(x, y) printf("\033[%d;%dH", (x), (y))
 
+static inline void setup()
+{
+	system("CHCP 932");
+    system("mode con: cols=80 lines=40");
+
+    printf(PROJECT_NAME);
+
+    printf("\nEnjoy. <Press ENTER to start>");
+
+    getchar();
+
+    system("cls");
+    printf("\nLoading...");
+
+    PlaySound("frames\\BA.wav", NULL, SND_ASYNC);
+    Sleep(500);
+    PlaySound(NULL, 0, 0);
+
+    system("cls");
+    rcrs(0, 1);
+}
+
 char *get_frame(const char *file_name)
 {
     FILE *f = fopen(file_name, "r");
@@ -45,38 +67,27 @@ char **get_all_frames()
     return frames;
 }
 
-static inline void setup()
-{
-    system("mode con: cols=80 lines=30");
-    printf(PROJECT_NAME);
-    printf("\nPlease turn down your volume.\n");
-    printf("\nEnjoy. <Press ENTER to start>");
-    getchar();
-}
-
 int main()
 {
-    clock_t now, last_frame;
+	int time_sec, count_t, min, sec, delta;
+    clock_t now, last_frame, start_time;
 
     char **frames = get_all_frames();
 
     setup();
 
-    system("cls");
-    printf("\nLoading...");
-
-    Sleep(500);
     PlaySound("frames\\BA.wav", NULL, SND_ASYNC);
 
-    system("cls");
-    rcrs(0, 1);
-
     last_frame = clock();
+    start_time = clock();
 
     for (int i = 1; i <= n_of_frames; i++)
     {
         now = clock();
-        int delta = now - last_frame;
+        delta = now - last_frame;
+		time_sec = (now - start_time) / CLOCKS_PER_SEC;
+        min = time_sec / 60;
+        sec = time_sec % 60;
 
         if (i % 30 == 0)
             last_frame += 43;
@@ -86,14 +97,31 @@ int main()
         if (delta < 33)
         {
             Sleep(33 - delta);
+
+            printf("\e[?25l");
+
+            printf("%s\n\n", PROJECT_NAME);
+
             printf("%s", frames[i - 1]);
+
+		    printf("\n ------------------------\n");
+			printf(" | Elapsed Time: %02d:%02d  |\n | Frame: %04d/%d     |", min, sec, i, n_of_frames);
+			printf("\n ------------------------\n");
+
+            rcrs(0, 1);
         }
     }
 
+    for(int i = 0; i < n_of_frames; i++)
+    {
+        free(frames[i]);
+    }
+    free(frames);
+
     Sleep(250);
 	system("cls");
-	printf("Thanks for watching.\n");
-	printf("<Press ENTER to exit>");
+	printf("Thank you for watching.\n");
+	printf("Press enter to exit.\n");
 	getchar();
 
     return 0;
